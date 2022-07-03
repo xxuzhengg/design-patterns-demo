6caa4d3c65233c9192d539a3ffa8fed32792ccb5
xuzheng <1580181628@qq.com>
2022-07-03 22:09:54 +0800

    设计模式

--- src/main/java/creational/factory/service/Shape.java
+++
@@ -1,5 +0,0 @@
-package creational.factory.service;
-
-public interface Shape {
-    void draw();
-}
--- src/main/java/structural/adapter/service/MediaPlayer.java
+++
@@ -1,8 +0,0 @@
-package structural.adapter.service;
-
-/**
- * 普通的媒体播放器
- */
-public interface MediaPlayer {
-    public void play(String audioType, String fileName);
-}
